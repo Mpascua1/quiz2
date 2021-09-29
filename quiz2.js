35e3436ac6c4800168b9074760b378bf99edf3c7
Mpascua1 <89545016+Mpascua1@users.noreply.github.com>
2021-09-29 13:48:59 -0600

    Add files via upload

---
+++ quiz2.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  document.getElementById("parhide").style.display = "none";
+}
+
+function myFunction1() {
+  document.getElementById("parhide").style.display = "block";
+}
